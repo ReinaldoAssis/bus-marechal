c0217ded57416d96b9675d3bc83490e47c87f905
Reinaldo Assis <randroi75@gmail.com>
2022-07-25 12:13:57 -0300

    add: map to viewroute

--- src/elements/Rotas/ViewRoute/ViewRoute.tsx
+++ src/elements/Rotas/ViewRoute/ViewRoute.tsx
@@ -1,8 +1,36 @@
-import { Title } from "@mantine/core";
+import { Badge, Group, Title } from "@mantine/core";
 import { useParams } from "react-router-dom";
-import { BusCardProps } from "../BusCard";
+import { BusCardIcon, BusCardProps } from "../BusCard";
+
+import { routes } from "../Data";
+import LiveMap from "../LiveMap";
 
 export default function ViewRoute() {
   const { id_arg } = useParams();
-  return <Title order={2}>{id_arg}</Title>;
+
+  const data: BusCardProps = routes.find((x) => x.id == id_arg) ?? {
+    showIcon: BusCardIcon.ShowIcon,
+    id: 505,
+    title: "Undefined - Error",
+  };
+
+  return (
+    <>
+      <div style={{ display: "flex" }}>
+        <Title order={2} style={{ marginRight: 30 }}>
+          {data.title}
+        </Title>
+        <Group>
+          {data.hours?.map((x) => {
+            return (
+              <Badge color="blue" variant="light">
+                {x}
+              </Badge>
+            );
+          })}
+        </Group>
+      </div>
+      <LiveMap Style={{ height: "80%", width: "100%", marginTop: 20 }} />
+    </>
+  );
 }
